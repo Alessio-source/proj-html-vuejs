463d099c62d2d1e6228a14ad0256c365c5a24335
Alessio Pancia <alessiop2801@gmail.com>
2021-01-13 11:57:49 +0100

    slide 1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.sass('src/style.scss', 'css');
+mix.sass('src/style.scss', 'css').options({
+    processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
+});
